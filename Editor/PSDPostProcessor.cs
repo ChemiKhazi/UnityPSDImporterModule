77fd75b15dc7a9a4fbbbb32c0731490d2c38860f
Jeiel Aranal <jeiel.aranal@gmail.com>
2015-09-08 00:12:33 +0800

    Changed how PSDPostProcessor.cs calls importer, a little hacky because of how AssetDatabase works

--- Editor/PSDPostProcessor.cs
+++ Editor/PSDPostProcessor.cs
@@ -9,16 +9,44 @@ namespace subjectnerdagreement.psdexport
 {
 	public class PSDPostProcessor : AssetPostprocessor
 	{
+		static List<Texture2D> reimports = new List<Texture2D>();
+
 		static void OnPostprocessAllAssets(string[] modified,
 											string[] deleted,
 											string[] moved,
 											string[] movedFrom)
 		{
+			reimports.Clear();
+
 			foreach (var modPath in modified)
 			{
 				if (modPath.ToLower().EndsWith(".psd") == false)
 					continue;
 				var target = AssetDatabase.LoadAssetAtPath<Texture2D>(modPath);
+				reimports.Add(target);
+			}
+
+			// This is kind of a hacky way of doing this,
+			// If there are PSDs that want to be reimported
+			// We have to do it outside of this function because
+			// AssetDatabase cannot handle new images created
+			// from within this function
+			if (reimports.Count > 0)
+			{
+				// So instead, hook into the editor update function
+				// and fire the import from there
+				EditorApplication.update += WaitForImport;
+			}
+		}
+
+		private static void WaitForImport()
+		{
+			// This function only fires once, unhook from update function immediately
+			EditorApplication.update -= WaitForImport;
+
+			for (int i = 0; i < reimports.Count; i++)
+			{
+				var target = reimports[i];
 				var exportSettings = new PsdExportSettings(target);
 				if (exportSettings.AutoReExport)
 				{
@@ -27,6 +55,8 @@ namespace subjectnerdagreement.psdexport
 					PSDExporter.Export(exportSettings, psdInfo);
 				}
 			}
+
+			reimports.Clear();
 		}
 	}
 }
